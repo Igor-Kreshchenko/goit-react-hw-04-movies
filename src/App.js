8fb2f55622955f0993c176e19ad0a749b620221b
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-06-07 12:35:29 +0300

    Реализовывает рендер стартовой страницы

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import NotFoundPage from './views/NotFoundPage';
+import styles from './App.module.css';
 
-class App extends Component {
-  
-  render() {
-    
-  }
-}
+const App = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className={styles.NavLink}
+            activeClassName={styles.NavLinkActive}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/movies"
+            className={styles.NavLink}
+            activeClassName={styles.NavLinkActive}
+          >
+            Movies
+          </NavLink>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/movies" component={MoviesPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
---
+++ src/views/HomePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchPopularMovies } from '../services/api-service';
+
+class HomePage extends Component {
+  state = {
+    movies: [],
+  };
+
+  async componentDidMount() {
+    const response = await fetchPopularMovies();
+    this.setState({ movies: response.data.results });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Popular Movies</h1>
+        <ul>
+          {this.state.movies.map(movie => (
+            <li key={movie.id}>
+              <Link to={`${this.props.match.url}/${movie.id}`}>
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default HomePage;
